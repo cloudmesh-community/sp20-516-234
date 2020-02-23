14adfb891d4da6a00aec12a22e361561b10adc75
Andrew Goldfarb <andrewgoldfarb@Andrews-MacBook-Pro.local>
2020-02-23 11:37:29 -0500

    renamed openAPI function and updated according references

--- openAPI/server.py
+++ openAPI/server.py
@@ -4,7 +4,7 @@ import connexion
 # Create the application instance
 app = connexion.App(__name__, specification_dir="./")
 # Read the yaml file to configure the endpoints
-app.add_api("timer.yaml")
+app.add_api("check_connect.yaml")
 
 
 # create a URL route in our application for "/"
